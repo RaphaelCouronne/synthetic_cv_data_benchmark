adf8920a02b72bf4c0103f6aa55a462c0e123ebe
Raphael Couronne <raphael.couronne@gmail.com>
2022-10-28 18:53:14 +0200

    Update prepareADE20K.py

--- data/prepareADE20K.py
+++ data/prepareADE20K.py
@@ -24,8 +24,18 @@ def subsample_ade20k(ade20k_folder, i):
             break
     ade20k_coco["annotations"] = ade20k_coco["annotations"][:j]
     
-    with open(osp.join(ade20k_folder, f"coco-training_{i}"), 'w') as fh:
+    with open(osp.join(ade20k_folder, f"coco-training_{i}.json"), 'w') as fh:
         json.dump(ade20k_coco, fh)
+
+    """
+    from mmcv import Config
+    from mmdet.datasets import build_dataset
+    ade_size = 512
+    cfg_data_ade20k = Config.fromfile("../../synthetic_cv_data_benchmark/datasets/ade20k.py")
+    cfg_data_ade20k.data.train.ann_file = f'../../../datasets/ADE20K_2021_17_01/coco-training_{ade_size}.json'
+    datasets = [build_dataset([cfg_data_ade20k.data.train])]
+    datasets[0].datasets
+    """
 
 ade20k_folder = "../../../datasets/ADE20K_2021_17_01/"
 
